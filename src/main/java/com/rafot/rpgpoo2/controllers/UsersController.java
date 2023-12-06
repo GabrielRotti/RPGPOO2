49d26e7de4044c4135958b80e7b6f50e407ec6a4
Gabriel Anceski <gabrielanceski@gmail.com>
2023-12-06 12:15:48 -0300

    chore: remove unused import

--- src/main/java/com/rafot/rpgpoo2/controllers/UsersController.java
+++ src/main/java/com/rafot/rpgpoo2/controllers/UsersController.java
@@ -10,7 +10,6 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Locale;
 
 @WebServlet(name = "userController", value = "/users")
 public class UsersController extends HttpServlet {
